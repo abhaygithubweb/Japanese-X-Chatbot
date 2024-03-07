9bfeee1d173e5e32ac05385b9243266f5332154a
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <156512147+nobitaaxd@users.noreply.github.com>
2024-03-07 11:16:26 +0530

    Update

---
+++ config.py
@@ -0,0 +1,16 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "6435225"
+# -------------------------------------------------------------
+API_HASH = "4e984ea35f854762dcde906dce426c2d"
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+MONGO_URL = getenv("MONGO_URL", None)
+OWNER_ID = "6694740726"
+SUPPORT_GRP = "Japanese_Userbot_Chat"
+UPDATE_CHNL = "Japanese_Userbot"
+OWNER_USERNAME = "Nobitaa_xd"
