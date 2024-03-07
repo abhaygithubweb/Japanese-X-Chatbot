31bc57b74fb8c97d0f89fa649abffe674e90eb8d
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <156512147+nobitaaxd@users.noreply.github.com>
2024-03-07 12:07:38 +0530

    Update stats.py

--- JAPANESE/modules/stats.py
+++ JAPANESE/modules/stats.py
@@ -1,1 +1,18 @@
+from pyrogram import filters, Client
+from pyrogram.types import Message
 
+from JAPANESE import OWNER, JAPANESE
+from JAPANESE.database.chats import get_served_chats
+from JAPANESE.database.users import get_served_users
+
+
+@JAPANESE.on_message(filters.command("stats") & filters.user(OWNER))
+async def stats(cli: Client, message: Message):
+    users = len(await get_served_users())
+    chats = len(await get_served_chats())
+    await message.reply_text(
+        f"""ᴛᴏᴛᴀʟ sᴛᴀᴛs ᴏғ {(await cli.get_me()).mention} :
+
+➻ **ᴄʜᴀᴛs :** {chats}
+➻ **ᴜsᴇʀs :** {users}"""
+    )
